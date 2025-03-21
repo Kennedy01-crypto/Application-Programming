10d1ac148b54d89268b704e5f0876bba7e15bfc4
Jim Kennedy <kennedyomata@gmail.com>
2025-03-21 16:22:43 +0300

    removed upper and lower bounds, added score functionality and bva

--- NumberGuessingGame/RandomNumberGuess.c
+++ NumberGuessingGame/RandomNumberGuess.c
@@ -1,64 +1,50 @@
-#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
-int main()
-{
-    int lower, upper, x, guess, count = 0, flag = 0;
-    int total_chances;
-
-    // Taking Inputs
-    printf("Enter Lower bound: ");
-    scanf("%d", &lower);
-
-    // Taking Inputs
-    printf("Enter Upper bound: ");
-    scanf("%d", &upper);
-
-    // Seed the random number generator
-    srand(time(0));
-
-    // Generating random number between the lower and upper
-    x = (rand() % (upper - lower + 1)) + lower;
-    total_chances
-        = (int)ceil(log(upper - lower + 1) / log(2));
-
-    printf("\n\tYou've only %d chances to guess the "
-           "integer!\n\n",
-           total_chances);
-
-    // for calculation of minimum number of guesses depends
-    // upon range
-    while (count < total_chances) {
-        count++;
-
-        // Taking guessing number as input
-        printf("Guess a number: ");
-        scanf("%d", &guess);
-
-        // Condition testing
-        if (x == guess) {
-            printf(
-                "Congratulations you did it in %d try!\n",
-                count);
-            // Once guessed, loop will break
-            flag = 1;
-            break;
-        }
-        else if (x > guess) {
-            printf("You guessed too small!\n");
-        }
-        else if (x < guess) {
-            printf("You guessed too high!\n");
-        }
+int calculateScore(int guessCounter) {
+    switch (guessCounter) {
+        case 1: return 100;
+        case 2: return 90;
+        case 3: return 80;
+        case 4: return 60;
+        case 5: return 40;
+        case 6: return 20;
+        case 7: return 5;
+        default: return 0;
     }
+}
 
-    // If Guessing is more than required guesses, shows this
-    // output.
-    if (!flag) {
-        printf("\nThe number is %d\n", x);
-        printf("\tBetter Luck Next time!\n");
+int main() {
+    int numberToGuess, chances = 7, guessCounter = 0, myGuess, myScore;
+
+    printf("Hi welcome to the game, This is a number guessing game.\n");
+    printf("You got 7 chances to guess the number. Let's start the game\n");
+
+    srand(time(NULL)); // Seed the random number generator
+    numberToGuess = rand() % 100; // Generate a random number between 0 and 99
+    printf("%d\n", numberToGuess); //for debuging
+
+    while (guessCounter < chances) {
+        guessCounter++;
+        printf("Please Enter your Guess(1 to 100): ");
+        scanf("%d", &myGuess);
+
+        if (myGuess == numberToGuess) {
+            myScore = calculateScore(guessCounter);
+            printf("The number is %d and you found it right !! in the %d attempt\n", numberToGuess, guessCounter);
+            printf("Your score is: %d\n", myScore);
+            break;
+        } else if (guessCounter >= chances) {
+            printf("Oops sorry, The number is %d better luck next time\n", numberToGuess);
+            break;
+        } else if ((myGuess > numberToGuess) && (myGuess < 101)) {
+            printf("Your guess is higher \n");
+        } else if ((myGuess < numberToGuess) && (myGuess > 0)) {
+            printf("Your guess is lesser\n");
+        } else{
+          printf("Number ot of bounds\n");
+        }
     }
 
     return 0;
