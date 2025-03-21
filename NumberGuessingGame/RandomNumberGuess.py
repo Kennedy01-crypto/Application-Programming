6c54346081e0a795ccdcf386437006abe7a54cad
Jim Kennedy <kennedyomata@gmail.com>
2025-03-21 16:01:10 +0300

    Added Boundary Value Analysis loogic and score functionality

--- NumberGuessingGame/RandomNumberGuess.py
+++ NumberGuessingGame/RandomNumberGuess.py
@@ -5,23 +5,43 @@ print("Hi welcome to the game, This is a number guessing game.\nYou got 7 chance
 number_to_guess = random.randrange(100)
 
 chances = 7
+guess_counter =  0 
 
-guess_counter = 0
+def calculate_score(guess_counter):
+  # Calculate the score based on the guess counter.
+  
+  scores = {
+    1: 100,
+    2: 90,
+    3: 80,
+    4: 60,
+    5: 40,
+    6: 20,
+    7: 5,
+  }
+  
+  return scores.get(guess_counter, 0) #returns the value if the key exists, otherwise 0.
+  
+# for debugging
+# print(number_to_guess)
 
 while guess_counter < chances:
-
-    guess_counter += 1
-    my_guess = int(input('Please Enter your Guess : '))
-
-    if my_guess == number_to_guess:
-        print(f'The number is {number_to_guess} and you found it right !! in the {guess_counter} attempt')
-        break
-
-    elif guess_counter >= chances and my_guess != number_to_guess:
-        print(f'Oops sorry, The number is {number_to_guess} better luck next time')
-
-    elif my_guess > number_to_guess:
-        print('Your guess is higher ')
-
-    elif my_guess < number_to_guess:
-        print('Your guess is lesser')
+  guess_counter += 1
+  my_guess = int(input('Please Enter your Guess (1 to 100):'))
+  
+  if my_guess == number_to_guess:
+    #calculate the score based on the guess_counter
+    myScore = calculate_score(guess_counter)
+    
+    print(f'The number is {number_to_guess}\nYou found it in attenpt {guess_counter}')
+    print(f'Your Score is: {myScore}')
+    break
+  elif guess_counter >= chances:
+    print(f'Oops sorry, The number is {number_to_guess} better luck next time')
+    break
+  elif (my_guess > number_to_guess and my_guess <= 100):
+    print("Your Guess is higher")
+  elif (my_guess < number_to_guess and my_guess >= 1):
+    print("Your Guess is lower")
+  else:
+    print("Number is not within the required range")
